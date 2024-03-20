3ded6f079ad014a84ae5ef415b1c7ba7f9c2134d
felixesteban100 <felixesteban100@gmail.com>
2024-03-20 09:47:18 -0400

    aded vercelServerless to astro.cofig.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({mode: 'standalone',}),
+  // adapter: node({mode: 'standalone',}),
+  adapter: vercelServerless(),
   integrations: [
     react(),
     tailwind({
