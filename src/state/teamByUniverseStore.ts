2ec6bc75405d71497f08d7c70c1d0b7ad75536fc
felixesteban100 <felixesteban100@gmail.com>
2024-03-23 21:02:40 -0400

    chage to svelte (the real rumbe) 2nd try

--- src/state/teamByUniverseStore.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'nanostores';
-
-export const teamByUniverseStore = atom(new Array())
-
-export function setTeamByUniverseStore(universe: string) {
-    // $users.set([...$users.get(), user]);
-}
--- src/state/teamStore.ts
+++
@@ -1,7 +0,0 @@
-import { computed } from 'nanostores'
-import { teamByUniverseStore } from './teamByUniverseStore'
-import { getTeamByUniverse } from '@/lib/constants'
-
-// export const teamStore = computed(teamByUniverseStore, () => {
-//     return getTeamByUniverse()
-// })
