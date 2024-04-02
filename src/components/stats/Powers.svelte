3cd45ae5982a8b159c581c0aa555291ebfb099f2
felixesteban100 <felixesteban100@gmail.com>
2024-04-02 07:23:35 -0400

    better responsiveness in the Stats component

--- src/components/stats/Powers.svelte
+++ src/components/stats/Powers.svelte
@@ -5,19 +5,29 @@
     export let powers: string[];
 </script>
 
-<div class="flex flex-col items-center justify-center h-5 mt-5 gap-5">
+<div class="flex flex-col items-center justify-center h-5 my-32 lg:mt-5 gap-5">
     <p class="font-bold text-primary text-xl">Main powers: </p>
-    <div class="flex justify-center items-center space-x-4">
+    <div class="hidden lg:flex flex-col lg:flex-row justify-center items-center space-x-4">
         {#if powers.length < 1}
         <span class=" font-bold text-xl">Powers unknown.</span>
         {/if}
+
         {#each powers as power, index }
-        <a href={`/characters?power=${power}`} class="hover:underline"><span>{power}</span></a>
+        <a href={`/characters?power=${power}`} class="hover:underline "><span>{power}</span></a>
             {#if index < powers.length - 1}
                 <Separator class="" orientation={"vertical"} />
             {/if}
         {/each}
     </div>
+    <div class="flex lg:hidden flex-col lg:flex-row justify-center items-center gap-8 ">
+      {#if powers.length < 1}
+      <span class=" font-bold text-xl">Powers unknown.</span>
+      {/if}
+
+      {#each powers as power }
+        <a href={`/characters?power=${power}`} class="hover:underline "><span>{power}</span></a>
+      {/each}
+  </div>
     <!-- <div>
         <div class="space-y-1">
           <h4 class="text-sm font-medium leading-none">Radix Primitives</h4>
--- src/components/stats/StatsNumber.svelte
+++ src/components/stats/StatsNumber.svelte
@@ -13,11 +13,11 @@
   </script>
    <!-- grid grid-cols-6 -->
    <!-- flex justify-between -->
-  <div class="w-full grid grid-cols-[100px_1fr_25px] items-center gap-10 my-5 ">
+  <div class="w-full grid grid-cols-2 lg:grid-cols-[100px_1fr_25px] items-center gap-10 my-5 ">
     <div class="flex items-center gap-2 ">
         <p class='text-xl lg:text-3xl'><slot name="icon"/></p>
-        <div class="fond-bold ">{statName}</div>
+        <div class="fond-bold hidden lg:flex">{statName}</div>
     </div>
-    <Progress {value} class="w-full " />
+    <Progress {value} class="w-full hidden lg:flex" />
     <p class="font-bold flex justify-end">{value}</p>
 </div>
