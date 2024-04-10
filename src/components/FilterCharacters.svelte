1c2ceaed700511d73ac7b49ee22895a94020db51
felixesteban100 <felixesteban100@gmail.com>
2024-04-10 09:46:03 -0400

    enter keydown shortcut added, and random_characters page added

--- src/components/FilterCharacters.svelte
+++ src/components/FilterCharacters.svelte
@@ -2,10 +2,18 @@
   import Button from "$lib/components/ui/button/button.svelte";
   import { Input } from "$lib/components/ui/input/index";
   import { Label } from "$lib/components/ui/label/index";
-  import { ALLALIGMENTS, ALLGENDERS, ALLPOWERS, ALLRACES, ALLUNIVERSE, SORT_BY_VALUES } from "../lib/constants/constants";
+  import {
+    ALLALIGMENTS,
+    ALLGENDERS,
+    ALLPOWERS,
+    ALLRACES,
+    ALLUNIVERSE,
+    SORT_BY_VALUES,
+  } from "../lib/constants/constants";
   import { Checkbox } from "../lib/components/ui/checkbox/index";
   import * as RadioGroup from "$lib/components/ui/radio-group/index";
   import type { WithId } from "mongodb";
+  import type { Universe } from "$lib/types";
 </script>
 
 <script lang="ts">
@@ -20,7 +28,7 @@
   export let power: string;
   export let universe: string;
   export let team: string;
-  export let universesWithTeams: WithId<Universe>[]
+  export let universesWithTeams: WithId<Universe>[];
 
   $: characterNameState = characterName;
   $: characterOrFullNameState = characterOrFullName;
@@ -33,26 +41,49 @@
   $: powerState = power;
   $: universeState = universe;
   $: teamState = team;
-  
-  $: universeInfo = universesWithTeams.find(c => c.value === universeState)
+
+  $: universeInfo = universesWithTeams.find((c) => c.value === universeState);
+
+  function goPage(
+    e: KeyboardEvent & {
+      currentTarget: EventTarget & Window;
+    },
+  ) {
+    // const isEnter: boolean = e.key == "Enter";
+    // const filter_characters_button = document.getElementById("filter_characters");
+    // if (isEnter) filter_characters_button?.click();
+
+    if (e.key === "Enter") {
+      // Prevent the default behavior (form submission, etc.)
+      e.preventDefault();
+      document.getElementById("filter_characters")?.click();
+    }
+  }
 </script>
 
 <div class="h-[850px] w-full overflow-y-scroll">
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
   <div class="flex flex-col gap-10">
     <Label class="flex flex-col gap-5">
-      CharacterName 
-      <Input 
-        id="name" 
-        placeholder="batman, superman, iron man, spider-man..." 
-        bind:value={characterNameState} 
+      CharacterName
+      <Input
+        id="name"
+        placeholder="batman, superman, iron man, spider-man..."
+        bind:value={characterNameState}
       />
     </Label>
-  
+
     <div class="flex flex-col gap-5">
-      <Label class="flex gap-5 items-center justify-start"><Checkbox bind:checked={characterOrFullNameState}/> Fullname (Checked) | CharacterName (Unchecked)  </Label>
-      <Label class="flex gap-5 items-center justify-start"><Checkbox bind:checked={includesOrExactState}/> Include charactes in name (checked) | Exact name (Unchecked) </Label>
+      <Label class="flex gap-5 items-center justify-start"
+        ><Checkbox bind:checked={characterOrFullNameState} /> Fullname (Checked)
+        | CharacterName (Unchecked)
+      </Label>
+      <Label class="flex gap-5 items-center justify-start"
+        ><Checkbox bind:checked={includesOrExactState} /> Include charactes in name
+        (checked) | Exact name (Unchecked)
+      </Label>
     </div>
-  
+
     <Label class="flex flex-col gap-5">
       Sort By
       <select
@@ -64,7 +95,7 @@
         {/each}
       </select>
     </Label>
-  
+
     <Label class="flex flex-col gap-5">
       Sort Direction
       <RadioGroup.Root bind:value={sortDirectionState}>
@@ -78,7 +109,7 @@
         </div>
       </RadioGroup.Root>
     </Label>
-    
+
     <Label class="flex flex-col gap-5">
       Gender
       <select
@@ -90,7 +121,7 @@
         {/each}
       </select>
     </Label>
-  
+
     <Label class="flex flex-col gap-5">
       Aligment
       <select
@@ -102,7 +133,7 @@
         {/each}
       </select>
     </Label>
-  
+
     <Label class="flex flex-col gap-5">
       Race
       <select
@@ -126,13 +157,13 @@
         {/each}
       </select>
     </Label>
-  
+
     <Label class="flex flex-col gap-5">
       Universe
       <select
         bind:value={universeState}
         on:change={() => {
-          teamState = "All"
+          teamState = "All";
         }}
         class="bg-background border border-foreground text-foreground text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5"
       >
@@ -141,30 +172,35 @@
         {/each}
       </select>
     </Label>
-  
+
     {#if universeInfo}
-        <Label class="flex flex-col gap-5">
-          Team
-          <select
-            bind:value={teamState}
-            class="bg-background border border-foreground text-foreground text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5"
-          >
-            {#each universeInfo.teams as team}
-              <option value={team.value}>{team.name}</option>
-            {/each}
-          </select>
-        </Label>
+      <Label class="flex flex-col gap-5">
+        Team
+        <select
+          bind:value={teamState}
+          class="bg-background border border-foreground text-foreground text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5"
+        >
+          {#each universeInfo.teams as team}
+            <option value={team.value}>{team.name}</option>
+          {/each}
+        </select>
+      </Label>
     {/if}
-  
+
     <!-- <p>name: {characterNameState}</p> -->
     <!-- <p>gender: {genderState}</p> -->
     <!-- <p>side: {sideState}</p> -->
     <!-- <p>universe: {universeState}</p> -->
-    <a href={`/characters?characterName=${characterNameState}&gender=${genderState}&side=${sideState}&race=${raceState}&power=${powerState}&universe=${universeState}&team=${teamState}&characterOrFullName=${characterOrFullNameState}&includesOrExact=${includesOrExactState}&sortBy=${sortByState}&sortDirection=${sortDirectionState}&currentPage=${1}`}>
-      <Button type="submit">Submit</Button>
+    <a
+      href={`/characters?characterName=${characterNameState}&gender=${genderState}&side=${sideState}&race=${raceState}&power=${powerState}&universe=${universeState}&team=${teamState}&characterOrFullName=${characterOrFullNameState}&includesOrExact=${includesOrExactState}&sortBy=${sortByState}&sortDirection=${sortDirectionState}&currentPage=${1}`}
+      class="flex justify-center items-center gap-5"
+    >
+      <Button id="filter_characters" type="submit">Submit</Button>
+      |
+      <p>Press 'ENTER'</p>
     </a>
   </div>
 </div>
 
-
-
+<!-- <svelte:window on:keydown|preventDefault={(e) => goPage(e)} /> -->
+<svelte:window on:keydown={goPage} />
