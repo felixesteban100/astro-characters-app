23f9e3b4797331a9e919dc400e35337a0379bbd5
felixesteban100 <felixesteban100@gmail.com>
2024-09-29 07:49:22 -0400

    lock position functionality added in random_characters page

--- src/components/random_characters/ChangeHowManyInput.svelte
+++ src/components/random_characters/ChangeHowManyInput.svelte
@@ -2,18 +2,22 @@
   import { Input } from "$lib/components/ui/input/index";
   import Button from "$lib/components/ui/button/button.svelte";
   import { navigate } from "astro:transitions/client";
+  import { Hash } from "lucide-svelte";
 </script>
 
 <script lang="ts">
   export let howMany: number;
   export let all: number[];
+  export let locked: number[];
 
   function changeHowMany() {
-    navigate(`/random_characters?all=${all.join("-")}&howMany=${howMany}`);
+    navigate(
+      `/random_characters?all=${all.join("-")}&howMany=${howMany}&locked=${locked.join("-")}`,
+    );
   }
 </script>
 
 <div class="flex gap-2">
   <Input class="w-[5rem]" bind:value={howMany} />
-  <Button on:click={changeHowMany}>Change</Button>
+  <Button size="icon" on:click={changeHowMany}><Hash /></Button>
 </div>
